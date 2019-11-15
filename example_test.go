9ed543b6bcb7bc476c92bf64ec87e18a5eb34b50
mobius <sv0220@163.com>
2019-11-15 10:16:24 +0800

    update example comment

--- example_test.go
+++ example_test.go
@@ -1,15 +1,19 @@
 package go_rbac
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+)
 
-func ExampleAuthorize() {
+// this example shows how to use this package to authorize sth.
+func ExampleUse_authorize() {
 	srvAddr := "127.0.0.1:6801"
 	c, err := New(srvAddr)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	rsp, err := c.Authorize(&Request{
+	rsp, err := c.Authorize(context.Background(), &Request{
 		Subject:  "ankr",
 		Resource: "/v1/path/to/rc",
 		Action:   "read",
